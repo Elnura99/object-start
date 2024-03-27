144517619d00caa067ac9240ed68a5c31382d22b
Elnura99 <elnuralizada99@mail.ru>
2024-03-27 19:40:23 +0400

    code updated

--- object.js
+++ object.js
@@ -34,7 +34,7 @@
 
 
 
-                                // TAPŞIRIQ
+                               // TAPŞIRIQ
                     //    -----tapşırıq(1,2,3,4,5)-----
 // const person = {
 //     name: 'Elnura',
@@ -81,38 +81,49 @@
 
 //  console.log(book);
 
+
  
                            // -----tapşırıq(10,11,12,13)-----
 
-const student = {
-    name: 'Amelia',
-    age:  16 ,
-    grades: [10, 15, 20, 25, 30]
-}
+// const student = {
+//     name: 'Amelia',
+//     age:  16 ,
+//     grades: [10, 15, 20, 25, 30]
+// }
 
 
 // delete student.grades
-console.log(student);
+// console.log(student);
 
-function getAvaregaGrade(b){
-    let sum = 0
-    for(let i=0; i< student.grades.length; i++){
-        sum+= student.grades[i]
+// function getAvaregaGrade(b){
+//     let sum = 0
+//     for(let i=0; i< student.grades.length; i++){
+//         sum+= student.grades[i]
         
-    }return sum/student.grades.length
-}
-console.log(getAvaregaGrade(student));
+//     }return sum/student.grades.length
+// }
+// console.log(getAvaregaGrade(student));
 
-                            // -----tapşırıq(14)-----
+
+
+
+                            // -----tapşırıq(14,15)-----
 
 // const rectangle = {
-//     width: '80%',
-//     height: 40
+//     width: 15,
+//     height: 20
 // }
 
 // console.log(rectangle);
 
+// function calculateArea(rectangle){
+//     return rectangle.width*rectangle.height
+// }
+// console.log(calculateArea(rectangle));
  
+
+
+
                             // -----tapşırıq(16)-----   
 
 // const circle = {
@@ -123,26 +134,40 @@ console.log(getAvaregaGrade(student));
 // console.log(circle.color);
 
 
-                           //   -----tapşırıq(18)-----
+
+
+                           //   -----tapşırıq(18,19)-----
 
 // const employee = {
-//     name: 'Elnara Rizvanova'
-//     position: 'developer'
+//     name: 'Elnara Rizvanova',
+//     position: 'developer',
 //     salary: 4000
 // }
 
+// function increase(employee) {
+//     return employee.salary * 10 / 100 + employee.salary
+// }
+// console.log(increase(employee));
 
 
-                             // -----tapşırıq(20)-----
+
+                             // -----tapşırıq(20,21)-----
 
 // const product = {
-//     name: 'telephone'
-//     price: 2000
+//     name: 'telephone',
+//     price: 2000,
 //     quantity: 9
 // }
 
+// function calculateTotalPrice(product){
+//     return product.price * product.quantity
+// }
+// console.log(calculateTotalPrice(product));
 
-                           //   -----tapşırıq(22)-----
+
+
+
+                           //   -----tapşırıq(22,23)-----
 
 // const movie = {
 //     title: 'Guardians of the Galaxy',
@@ -153,3 +178,53 @@ console.log(getAvaregaGrade(student));
 // movie.year = 2023
 
 // console.log(movie);
+
+
+
+                             // -----tapşırıq(24,25)-----
+
+// const bankAccount = {
+//    balance: 1400,
+//    currency: 'TRY'
+// }
+
+// function increaseBalance(bankAccount){
+//     return bankAccount.balance - 350
+// }
+// console.log(increaseBalance(bankAccount));
+
+
+
+
+                           // -----tapşırıq(26,27,28,29)-----
+
+// const team = {
+//     name: 'Galatasaray',
+//     coach: 'Okan Buruk',
+//     players: ['Mauro Icardi', 'Muslera']
+// }
+
+// team.players.push('Hakim Ziyax')
+// delete team.coach
+// console.log(team);
+
+// function printTeamInfo(a) {
+//     for(let [key, value] of Object.entries(team)){
+//     console.log(key, value);
+//     }
+// }
+
+// printTeamInfo(team)
+
+
+                              // -----tapşırıq(30)-----
+
+// const city = {
+//     name: 'Jacob',
+//     population: 5100000,
+//     country: 'Seul'
+// }
+
+// console.log(Object.keys(city));
+// console.log(Object.values(city));
+// console.log(Object.entries(city));
